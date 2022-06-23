42d1f61ace10de7faa745affaf422beef0aca85e
Daniel Antonio <91715315+DaMar4@users.noreply.github.com>
2022-06-23 04:06:04 -0500

    Add files via upload

---
+++ fin.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xrandr --output eDP --mode 1366x768 --pos 277x156 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+nitrogen --restore
+~/.config/openbox/polybar/launch.sh &
