087254a8ee09059041759fce7cf09422567d9f82
Daniel Antonio <91715315+DaMar4@users.noreply.github.com>
2022-07-03 21:55:05 -0500

    Update fin.sh

--- fin.sh
+++ fin.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-xrandr --output eDP --mode 1366x768 --pos 277x156 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+#xrandr --output eDP --mode 1366x768 --pos 277x156 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output eDP --mode 1366x768 --pos 1920x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
 nitrogen --restore
 ~/.config/openbox/polybar/launch.sh &
